fb28e52a7e06c7faf666d6604c9fe2834946bfa7
Cody Collier <cody@telnet.org>
2014-04-24 22:20:07 -0500

    host command which lightly mimics the traditional unix host utility

---
+++ host.go
@@ -0,0 +1,39 @@
+//
+// host - resolve a dns name to an ip
+//
+// Lightly mimics the unix host command
+//
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+	"os"
+)
+
+var host string
+
+func main() {
+
+	flag.Parse()
+
+	if flag.NArg() != 1 {
+		fmt.Printf("Usage: %s <hostname>\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	host := flag.Arg(0)
+
+	iplist, err := net.LookupHost(host)
+
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		os.Exit(1)
+	}
+
+	for _, ip := range iplist {
+		fmt.Printf("%s has address %s\n", host, ip)
+	}
+
+}
