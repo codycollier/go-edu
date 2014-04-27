98d684a293338762440682e554cf2add0ba5e439
Cody Collier <cody@telnet.org>
2014-04-26 23:45:24 -0500

    more work with box

--- boxy.go
+++ boxy.go
@@ -1,4 +1,4 @@
-// boxy is some testing with the NaCl libs in go
+// boxy - working with the NaCl libs in go
 package main
 
 import (
@@ -14,16 +14,16 @@ func main() {
 	if err != nil {
 		fmt.Printf("Error: %s", err)
 	}
-	fmt.Printf(" public key:\n%x\n", *pubkey)
-	fmt.Printf("private key:\n%x\n", *prvkey)
+	fmt.Printf(" public key (hex):\n%x\n", *pubkey)
+	fmt.Printf("private key (hex):\n%x\n", *prvkey)
 
 	hash256 := sha256.New()
 
 	hash256.Write(pubkey[:])
-	fmt.Printf("hash  public key:\n%x\n", hash256.Sum(nil))
+	fmt.Printf(" public key (sha256 hash):\n%x\n", hash256.Sum(nil))
 
 	hash256.Reset()
 	hash256.Write(prvkey[:])
-	fmt.Printf("hash private key:\n%x\n", hash256.Sum(nil))
+	fmt.Printf("private key (sha256 hash):\n%x\n", hash256.Sum(nil))
 
 }
