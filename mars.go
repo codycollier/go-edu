473fe774e1588903bc5ba505a0d038f8eb771b25
Cody Collier <cody@telnet.org>
2014-05-26 18:12:50 -0500

    working with json marshalling and unmarshalling

--- mars.go
+++ mars.go
@@ -5,19 +5,37 @@ import (
 	"fmt"
 )
 
+//
+// Unmarshal
+//
+
 type message1 struct {
 	name  string
 	mtype string
 }
 
 type message2 struct {
-	Name  string
-	Mtype string "Type"
+	Name   string `json:"name"`
+	Chtype string `json:"Type"`
 }
 
 type message3 struct {
 	Name string
 	Type string
+}
+
+//
+// Marshal
+//
+
+type messageA struct {
+	name   string `json:"name"`
+	chtype string `json:"type"`
+}
+
+type messageB struct {
+	Name   string `json:"name"`
+	ChType string `json:"type"`
 }
 
 func main() {
@@ -26,6 +44,13 @@ func main() {
 	var j1m1 message1
 	var j1m2 message2
 	var j1m3 message3
+	var m0 message3
+
+	fmt.Println("")
+	fmt.Println("--------------------------------------------------------")
+	fmt.Println(":: unmarshal")
+	fmt.Println("--------------------------------------------------------")
+	fmt.Println("")
 
 	j1 = []byte(`{"name":"namej1", "type":"typej1"}`)
 	fmt.Println("The initial json:")
@@ -42,8 +67,32 @@ func main() {
 
 	fmt.Println("")
 	fmt.Println("The desired struct:")
-	m0 := message2{"namej1", "typej1"}
+	m0 = message3{"namej1", "typej1"}
 	fmt.Printf("          : %s\n", m0)
 
+	fmt.Println("")
+	fmt.Println("--------------------------------------------------------")
+	fmt.Println(":: marshal")
+	fmt.Println("--------------------------------------------------------")
+	fmt.Println("")
+
+	var jA []byte
+	var mA messageA
+	var mB messageB
+
+	mA = messageA{"namemA", "typemA"}
+	mB = messageB{"namemB", "typemB"}
+	fmt.Println("The initial struct...")
+	fmt.Printf("mA: %s\n", mA)
+	fmt.Printf("mB: %s\n", mB)
+
+	jA, _ = json.Marshal(mA)
+	jA, _ = json.Marshal(mB)
+	fmt.Println("")
+	fmt.Println("The struct marshaled in to json:")
+	fmt.Printf("mA into json: %s\n", jA)
+	fmt.Printf("mB into json: %s\n", jA)
+
+	fmt.Println("")
 	fmt.Println("")
 }
