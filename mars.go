1ff1792165f961a51c64666e678b7b693882ebba
Cody Collier <cody@telnet.org>
2014-05-26 17:23:25 -0500

    working with json decoding

--- mars.go
+++ mars.go
@@ -12,31 +12,38 @@ type message1 struct {
 
 type message2 struct {
 	Name  string
-	Mtype string
+	Mtype string "Type"
+}
+
+type message3 struct {
+	Name string
+	Type string
 }
 
 func main() {
-	var err error
+
 	var j1 []byte
-	var j2 []byte
-	var m1 message1
-	var m2 message2
+	var j1m1 message1
+	var j1m2 message2
+	var j1m3 message3
 
-	j1 = []byte(`{"name":"green", "type":"color"}`)
-	j2 = []byte(`{"name":"green", "type":"color"}`)
+	j1 = []byte(`{"name":"namej1", "type":"typej1"}`)
+	fmt.Println("The initial json:")
 	fmt.Printf("j1: %s\n", j1)
-	fmt.Printf("j2: %s\n", j2)
 
-	err = json.Unmarshal(j1, &m1)
-	if err != nil {
-		fmt.Printf("err: %s", err)
-	}
-	fmt.Printf("m1: %s\n", m1)
+	json.Unmarshal(j1, &j1m1)
+	json.Unmarshal(j1, &j1m2)
+	json.Unmarshal(j1, &j1m3)
+	fmt.Println("")
+	fmt.Println("The json unmarshaled in to structs:")
+	fmt.Printf("j1 into m1: %s\n", j1m1)
+	fmt.Printf("j1 into m2: %s\n", j1m2)
+	fmt.Printf("j1 into m3: %s\n", j1m3)
 
-	err = json.Unmarshal(j2, &m2)
-	if err != nil {
-		fmt.Printf("err: %s", err)
-	}
-	fmt.Printf("m2: %s\n", m2)
+	fmt.Println("")
+	fmt.Println("The desired struct:")
+	m0 := message2{"namej1", "typej1"}
+	fmt.Printf("          : %s\n", m0)
 
+	fmt.Println("")
 }
