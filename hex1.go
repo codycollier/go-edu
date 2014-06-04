46ac14586789502a903530a7a62b7aa5fafc19a2
Cody Collier <cody@telnet.org>
2014-06-04 01:01:01 -0500

    fix fmt string

--- hex1.go
+++ hex1.go
@@ -10,7 +10,7 @@ func main() {
 	fmt.Printf("csid_string: %s\n", csid_string)
 
 	csid_byte, _ := hex.DecodeString(csid_string)
-	fmt.Printf("len(csid_byte): %s\n", len(csid_byte))
+	fmt.Printf("len(csid_byte): %v\n", len(csid_byte))
 
 	csid_string_again := hex.EncodeToString(csid_byte)
 	fmt.Printf("csid_string: %s\n", csid_string_again)
